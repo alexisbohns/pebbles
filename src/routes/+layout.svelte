47cc636d5b335716bf15d1762c958f9f35bf443d
Bohns <hello@bohns.design>
2025-10-03 23:46:49 +0200

    implement vercel insights script (#2)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	
+	injectSpeedInsights();
 </script>
 
 <svelte:head>
