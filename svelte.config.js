9c8190214fdc629ee1380410e90d6ebf5fce6ae9
Bohns <hello@bohns.design>
2025-10-04 01:18:29 +0200

    init stylus

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			stylus: {}
+		})
+	],
 	kit: { adapter: adapter() }
 };
 
