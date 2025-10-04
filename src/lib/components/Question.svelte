43212746a76490e08d7ac86161aa85a00c2f87c5
Bohns <hello@bohns.design>
2025-10-04 17:06:30 +0200

    introduce question component

---
+++ src/lib/components/Question.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { t } from '$lib';
+
+	export let name: string;
+	export let question: string;
+	export let description: string;
+	export let placeholder: string;
+	export let value: string = '';
+
+	let hintId: string;
+	$: hintId = `${name}-hint`;
+</script>
+
+<label for={name}>{$t(question)}</label>
+<p id={hintId}>{$t(description)}</p>
+<input
+	id={name}
+	{name}
+	type="text"
+	aria-describedby={hintId}
+	placeholder={$t(placeholder)}
+	bind:value
+/>
