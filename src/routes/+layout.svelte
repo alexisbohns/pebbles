016c44b51bddc6ff01c2d9d2937d82eb78b2ca20
Bohns <hello@bohns.design>
2025-10-04 16:46:56 +0200

    init global style and introduce alegreya standalone font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-
+	import '../global.styl';
+	
 	let { children } = $props();
 
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
