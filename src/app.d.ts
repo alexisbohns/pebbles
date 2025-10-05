23dee8ef3da57cf5d62f3a39bdc6927166f38fce
Bohns <hello@bohns.design>
2025-10-05 22:09:12 +0200

    setup supabase ssr client and dummy page (#38)

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,13 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
+
+import type { SupabaseClient, User } from '@supabase/supabase-js';
+
 declare global {
 	namespace App {
+		interface Locals {
+			supabase: SupabaseClient;
+			user: User | null;
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
---
+++ src/routes/test/+page.server.ts
@@ -0,0 +1,9 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const {
+		data: { user }
+	} = await locals.supabase.auth.getUser();
+
+	return { user };
+};
