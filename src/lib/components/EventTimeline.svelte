23cd9f6d0ca585104619f64ef8ed59b78e468a3c
Bohns <hello@bohns.design>
2025-10-17 00:44:13 +0200

    add a create button that leads to create page

--- src/lib/components/EventTimeline.svelte
+++ src/lib/components/EventTimeline.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import EventTimelineItem from './EventTimelineItem.svelte';
 	import type { EventSummary } from '$lib/types/events';
 	import { t } from '$lib';
@@ -7,10 +8,14 @@
 
 	const heading = $derived($t('timeline.title'));
 	const emptyLabel = $derived($t('timeline.empty'));
+	const createLabel = $derived($t('timeline.create'));
 </script>
 
 <section>
-	<h1>{heading}</h1>
+	<header>
+		<h1>{heading}</h1>
+		<a href={resolve('/create')}>{createLabel}</a>
+	</header>
 	{#if events.length === 0}
 		<p>{emptyLabel}</p>
 	{:else}
