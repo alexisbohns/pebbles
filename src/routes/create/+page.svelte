ea13ee874d772a2ebfb06ef826e5828839ea7a71
Bohns <hello@bohns.design>
2025-10-23 22:10:19 +0200

    implement an image in create module on crreate route (#96)

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -2,6 +2,7 @@
 	import { resolve } from '$app/paths';
 	import { t } from '$lib';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import testImage from '$lib/assets/test.png';
 
 	export let data;
 	const templates = data.templates;
@@ -10,17 +11,22 @@
 <h1>{$t('create.title')}</h1>
 {#each templates as template (template.name)}
 	<div class="bg-card border p-4 rounded-lg mb-4 flex flex-col gap-4 sm:flex-row sm:items-center">
-		<div class="flex grow flex-col">
-			<span>
-				{template.label}
-			</span>
-			<span class="text-sm text-muted-foreground">
-				{template.description}
-			</span>
-		</div>
-		<div class="flex flex-shrink-0 gap-2">
-			<Button href={resolve(`/create/${template.name}`)}>Créer</Button>
-			<Button variant="outline" href={resolve(`/create/${template.name}/step/0`)}>Pas à pas</Button>
+		<img src={testImage} alt={template.label} class="w-32 flex-shrink-0" />
+		<div class="flex grow flex-col gap-3">
+			<div class="flex grow flex-col">
+				<span class="font-semibold">
+					{template.label}
+				</span>
+				<span class="text-sm text-muted-foreground">
+					{template.description}
+				</span>
+			</div>
+			<div class="flex flex-shrink-0 gap-2">
+				<Button href={resolve(`/create/${template.name}`)}>Créer</Button>
+				<Button variant="outline" href={resolve(`/create/${template.name}/step/0`)}
+					>Pas à pas</Button
+				>
+			</div>
 		</div>
 	</div>
 {/each}
