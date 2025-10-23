dc30a13941704383db7e792dcc10282061258868
Bohns <hello@bohns.design>
2025-10-23 23:51:01 +0200

    remove paint font (#97)
    * remove paint font
    
    * reduce fonts and add vercel script
    
    * add vercel script
    
    * format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import EventTimeline from '$lib/components/EventTimeline.svelte';
 	import LandingContent from '$lib/components/LandingContent.svelte';
 	import type { PageData } from './$types';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { data }: { data: PageData } = $props();
 	const events = data.events ?? [];
