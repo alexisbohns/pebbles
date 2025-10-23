9f722caac4d7c9c32a2c66db8b6df3b82e92ebca
Bohns <hello@bohns.design>
2025-10-23 23:36:38 +0200

    reduce fonts and add vercel script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import EventTimeline from '$lib/components/EventTimeline.svelte';
 	import LandingContent from '$lib/components/LandingContent.svelte';
 	import type { PageData } from './$types';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 
 	let { data }: { data: PageData } = $props();
 	const events = data.events ?? [];
