b7160c0b4bcf39475ca89ccc19d4ec2d5c3d25de
Bohns <hello@bohns.design>
2025-10-25 09:41:49 +0200

    config the pwa manifest (#99)
    * config the pwa manifest
    
    * add iOS and android meta

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			strategies: 'generateSW',
+			registerType: 'autoUpdate',
+			includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
+			manifest: {
+				name: 'Pebbles',
+				short_name: 'Pebbles',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				background_color: '#ffffff',
+				theme_color: '#222222',
+				icons: [
+					{
+						src: '/icons/icon-192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/icons/icon-512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	]
 });
