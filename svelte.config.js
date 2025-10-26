15095708fd6aff77399b07433c83f56f0154006c
Bohns <hello@bohns.design>
2025-10-26 22:03:18 +0100

    fix useless preprocess error raised by linter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
